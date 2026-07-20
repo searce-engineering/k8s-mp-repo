723d1013d08ba78d9b06c969a92231465577c0fb
Darshan Gohel <darshan.gohel@searce.com>
2026-07-20 15:22:17 +0530

    bug/fix schema.yaml

---
+++ _help.sh
@@ -0,0 +1,36 @@
+##  App  Image ##
+## ## ## ## ## ##
+
+## Linux
+docker buildx build \
+  --provenance=false \
+  --sbom=false \
+  --no-cache \
+  -t app:1.0.0 \
+  ./app
+
+## Deployer Image ##
+## ## ## ## ## ## ##
+
+## Linux
+docker buildx build \
+  --provenance=false \
+  --sbom=false \
+  --no-cache \
+  -f deployer/Dockerfile \
+  -t deployer:1.0.0 \
+  .
+
+# Tag App Images
+docker tag app:1.0.0 us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app:1.0
+docker tag app:1.0.0 us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app:1.0.0
+
+# Tag Deployer Images
+docker tag deployer:1.0.0 us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app/deployer:1.0
+docker tag deployer:1.0.0 us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app/deployer:1.0.0
+
+#Push to Artifact Registry
+docker push us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app:1.0
+docker push us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app:1.0.0
+docker push us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app/deployer:1.0
+docker push us-docker.pkg.dev/searce-cloud-products/gcp-mp-poc-k8s/app/deployer:1.0.0
